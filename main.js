219552751e8cb0981d63da9f1f9cb709102413a0
jonwsantos <jonataswill.jwds@gmail.com>
2022-11-15 16:58:17 -0300

    atualização 2

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
-    $('form button').click(function() {
-    })
 
     $('form').on('submit', function(e) {
         e.preventDefault();
+        const novaTarefa = $('#nova-tarefa').val();
+        const novoItem = $('<li style="list-style: disc"></li>');
 
-        const novaTarefa = $('#nome-tarefa').val();
-        const tarefaAdicionada = $(`<li>${novaTarefa}</li>`);
-        $(novaTarefa).appendTo(tarefaAdicionada);
-        $(tarefaAdicionada).appendTo('ul');
-        $('#nome-tarefa').val('');
+        $(`<li>${novaTarefa}</li>`).appendTo(novoItem);
+        $(novoItem).appendTo(`ul`);
+        $('#nova-tarefa').val('');
+
+        $(novoItem).click(function() {
+            $(novoItem).css("text-decoration", "line-through");
+        })
     })
 })
